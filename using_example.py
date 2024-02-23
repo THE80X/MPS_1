9d4a9efa7199de932b719aa12925ccc0548d25cb
THE80X <ivansolovev1488@gmail.com>
2024-02-23 12:25:29 +0300

    final_version

--- using_example.py
+++ using_example.py
@@ -3,11 +3,11 @@ import numpy as np
 from DecisionHelper import DecisionHelper
 
 if __name__ == '__main__':
-    m = np.array([[3, 3, 3, 5, 3, 2],
-                  [3, 3, 3, 4, 3, 2],
-                  [3, 2, 3, 3, 3, 2]])
+    m = np.array([[8, 7, 6, 7, 8, 9],
+                  [9, 8, 7, 8, 9, 8],
+                  [7, 6, 8, 6, 7, 7]])
     wei = [0.1, 0.3, 0.1, 0.2, 0.2, 0.1]
-
+    map_of_the_table = ['GCC', 'Clang', 'MSVC']
     dh = DecisionHelper(m, wei)
     print(dh.saw())
     print(dh.topsis())
